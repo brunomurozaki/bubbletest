7291f927e11ffbb073327a16e05c3de6d340b6db
Bruno Murozaki <you@example.com>
2017-04-03 21:45:15 -0300

    Adding npm start to run heroku

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 var tst = 0;
+
+
+//Lets require/import the HTTP module
+var http = require('http'),
+fs = require('fs');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    
+    if(request.url.length == 1)
+    {
+    	fs.readFile('/index.html', function (err, html) {
+		    if (err) {
+		        throw err; 
+		    }
+
+		    response.write(html);
+		});
+    }
+
+    
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
