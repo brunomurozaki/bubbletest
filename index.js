98a2db271b605bde3f44cec0c54ce65adc96c9d2
Bruno Murozaki <you@example.com>
2017-04-03 21:58:04 -0300

    Adding the ENV port to work on Heroku

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ var server = http.createServer(handleRequest);
 
 //Lets start our server
 server.listen(PORT, function(){
+
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log("Server listening on: http://localhost:%s", process.env.PORT || PORT);
 });
