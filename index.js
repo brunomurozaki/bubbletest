c76b74db88277c7961ab59db9d1523df814d08a5
Bruno Murozaki <you@example.com>
2017-04-25 14:05:42 -0300

    Fixing the ejs thing

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', function(req, res) {
 
-    // ejs render automatically looks in the views folder
-    res.render('index');
+    res.send('hello world');
 });
 
 app.listen(port, function() {
