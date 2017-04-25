f6794a5550ff570de818ae16c3cf87b9c2a01b3b
Bruno Murozaki <you@example.com>
2017-04-25 14:04:12 -0300

    Fixing the ejs thing

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
