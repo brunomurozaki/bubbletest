5f795a52052f36b14f7abc45c91205ed972912ed
Bruno Murozaki <you@example.com>
2017-04-25 14:14:23 -0300

    Testing the static app

--- public/index.js
+++ public/index.js
@@ -5,15 +5,17 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
+
 
 // set the home page route
 app.get('/', function(req, res) {
 
     res.sendFile(__dirname + '/index.html');
 });
+
+
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
