c4890af858261a740c6d4bef83fad6e8d1eebaf4
Bruno Murozaki <you@example.com>
2017-04-25 14:00:24 -0300

    adding a small test of the express into the server

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
