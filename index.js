43a947b2dca3a5f5f8fa835fecb8ccdd4588a984
Bruno Murozaki <you@example.com>
2017-04-25 14:07:15 -0300

    Testing the indexhtml sending

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', function(req, res) {
 
-    res.send('hello world');
+    res.sendFile('index.html');
 });
 
 app.listen(port, function() {
