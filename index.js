38712a7723f7c380370002a5b176d39011ad3435
Bruno Murozaki <you@example.com>
2017-05-30 11:20:27 -0300

    Server REST Implementation

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/friends_data', function (req, res){
-	console.log(req.data);
+	console.log(req.friends_data);
 });
 
 app.get('/match_likes', function (req, res) {
