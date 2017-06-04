d22229657b78269e0ebe29e68957fc952fa3cbf8
Bruno Murozaki <you@example.com>
2017-06-04 12:35:00 -0300

    initial implementation of yaml editor

---
+++ public/js/editPages.js
@@ -0,0 +1,56 @@
+/*Js for editing yaml pages*/
+
+$(function(){
+	init();
+});
+
+
+var trackedPages = {};
+var trackedPagesKeys = {};
+
+
+function init(){
+	getPagesData();
+}
+
+function addPageRow(pageKey, page) {
+	var row = $("<div>"),addClass("formRow");
+	var rowTitle = $("<h5>").addClass("rowTitle");
+	var component, componentTitle, componentValue;
+	var itens = Object.keys(page);
+	var item, itemKey;
+
+	row.append(rowTitle);
+	rowTitle.append(pageKey);
+
+	for(var i = 0; i < itens.length; i++){
+		itemKey = itens[i];
+		item = page[itemKey];
+
+		component = $("<div>").addClass("component");
+		componentTitle = $("<div>").addClass("componentTitle");
+		componentValue = $("<div>").addClass("componentValue");
+		
+		componentTitle.append(itemKey);
+		componentValue.append(item);
+		component.append(componentTitle);
+		component.append(componentValue);
+
+		row.append(component);
+	}
+
+	$("#editForm").append(row);
+}
+
+function mountDataArray() {
+	var page, pageKey;
+
+	for(var i = 0; i < trackedPagesKeys.length; i++){
+		pageKey = trackedPagesKeys[i];
+		page = trackedPages[trackedPagesKeys[i]];
+		addPageRow(pageKey, page);
+	}
+
+	//separateWingData();
+	//mountMap();	
+}
