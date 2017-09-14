86a5602d23a3afbe9eec4d070b6b881c85119ac0
Bruno Murozaki <brunoimurozaki@gmail.com>
2017-09-14 20:47:15 -0300

    Taking out the PG code

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-pg.defaults.ssl = false;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres!');
-  createBasicDatabase(client);
-
-});
-
 var pages = {};
 var friends_data = {};
 var port = process.env.PORT || 8080;
@@ -41,37 +33,6 @@ app.get('/pages_data', function (req, res){
 	res.send(JSON.stringify(pages));
 });
 
-app.get('/del_user', function (req, res){
-	var fb_id = req.query.fb_id;
-	delFbId(fb_id);
-	res.sendStatus(200);
-});
-
-app.get('/add_user', function (req, res){
-	var fb_id = req.query.fb_id;
-	addFbId(fb_id);
-	res.sendStatus(200);
-});
-
-app.get('/get_users', function (req, res){
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		const results = [];
-		if (err) throw err;
-		var query = "SELECT fb_id from users;";
-		client.query(query)
-			.on("row", function(row){
-				results.push(row.fb_id);
-			})
-			.on("error", function(error){
-				console.log(error);
-			})
-			.on("end", function(){
-				done();
-				return res.json(results);
-		  	});
-	});
-});
-
 app.listen(port, host, function() {
     console.log('Our app is running on http://' + host + ':' + port);
 	console.log("is it really running?");
@@ -88,36 +49,3 @@ function readImportantPages(){
 }
 
 readImportantPages();
-
-function createBasicDatabase(client){
-	var tableQuery = "create table IF NOT EXISTS users(fb_id CHAR(50) PRIMARY KEY NOT NULL );"
-	client.query(tableQuery);
-}
-
-function delFbId(fb_id){
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	  if (err) throw err;
-	  var query = "DELETE FROM users WHERE fb_id = '" + fb_id + "';";
-	  client.query(query)
-	  	.on("error", function(error){
-	  		console.log(error);
-	  	})
-	  	.on("end", function(){
-	  		done();
-	  	});
-	});
-}
-
-function addFbId(fb_id){
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	  if (err) throw err;
-	  var query = "INSERT INTO users VALUES('" + fb_id + "');";
-	  client.query(query)
-	  	.on("error", function(error){
-	  		console.log(error);
-	  	})
-	  	.on("end", function(){
-	  		done();
-	  	});
-	});
-}
